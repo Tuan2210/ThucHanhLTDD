ed700c6b10339b67d1e3456d7a45beaafa7862bd
Tuan2210 <84191285+Tuan2210@users.noreply.github.com>
2022-02-23 11:22:36 +0700

    update pass lap2

---
+++ Lap02/app/src/main/java/com/example/lap02/activity_2a.java
@@ -0,0 +1,32 @@
+package com.example.lap02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class activity_2a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2a);
+
+        Button btnLogin  = findViewById(R.id.btnLogin);
+        EditText edtName = findViewById(R.id.edtName);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtName.getText().equals("")){
+                    Toast.makeText(activity_2a.this, "Not thing to show", Toast.LENGTH_SHORT).show();
+                }
+                else Toast.makeText(activity_2a.this, edtName.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}

d7e4a2701eeeba128f537f7762c00fe25aca44aa
Tuan2210 <84191285+Tuan2210@users.noreply.github.com>
2022-03-08 23:51:03 +0700

    upload lap3c_screen1

---
+++ Lap03/app/src/main/java/com/example/lap03/activity_3a.java
@@ -0,0 +1,14 @@
+package com.example.lap03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_3a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3a);
+    }
+}

69dcb3ce0632199b0dedb368d1fa631d879adc22
Tuan2210 <84191285+Tuan2210@users.noreply.github.com>
2022-04-05 08:51:11 +0700

    upload lap6_tiki

---
+++ Tiki_Fragment/app/src/main/java/com/example/tiki_fragment/FragmentAbove.java
@@ -0,0 +1,19 @@
+package com.example.tiki_fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentAbove extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View aboveView = inflater.inflate(R.layout.fragment_above, container, false);
+        return aboveView;
+    }
+}
---
+++ Tiki_Fragment/app/src/main/java/com/example/tiki_fragment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tiki_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                                    .add(R.id.fragmentContainerView1, new FragmentAbove(), "fragmentAbove")
+                                    .commit();
+
+        getSupportFragmentManager().beginTransaction()
+                                    .add(R.id.fragmentContainerView2, new FragmentBelow(), "fragmentBelow")
+                                    .commit();
+    }
+}

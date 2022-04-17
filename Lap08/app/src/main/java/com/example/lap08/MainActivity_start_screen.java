49b3c8d77f12bcb60d62a0d55118c6581ca0b7ed
Tuan2210 <84191285+Tuan2210@users.noreply.github.com>
2022-04-17 12:14:45 +0700

    upload full frame UI

--- Lap08/app/src/main/java/com/example/lap08/MainActivity_start_screen.java
+++ Lap08/app/src/main/java/com/example/lap08/MainActivity_start_screen.java
@@ -2,7 +2,9 @@ package com.example.lap08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity_start_screen extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity_start_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_start_screen);
+
+        //btnSignIn
+        findViewById(R.id.btnSignIn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity_start_screen.this.startActivity(new Intent(MainActivity_start_screen.this, MainActivity_sign_in_screen.class));
+            }
+        });
+
+        //btnRegister
+        findViewById(R.id.btnRegister1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity_start_screen.this.startActivity(new Intent(MainActivity_start_screen.this, MainActivity_register_screen.class));
+            }
+        });
     }
 }
